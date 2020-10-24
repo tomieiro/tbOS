f573cba0dd5aaab0f6ec3b20fc32e612ff40aefd
matrigato <51022834+matrigato@users.noreply.github.com>
2020-10-23 23:46:34 -0300

    Added .gitignore

--- kernel.asm
+++ kernel.asm
@@ -1,25 +1,25 @@
 BITS 16
 
-section .text         
+section .text
 global KERNEL
 
 KERNEL:
 	mov sp, 0x8000
 	mov ax, 07C0h
 	mov ds, ax
-    mov ax, 0x0E52
-    int 0x10
-    mov ax, 0x0E69
-    int 0x10
-    mov ax, 0x0E67
-    int 0x10
-    mov ax, 0x0E61
-    int 0x10
-    mov ax, 0x0E74
-    int 0x10
-    mov ax, 0x0E6f
-    int 0x10
-    cli
-    hlt
+	mov ax, 0x0E52
+	int 0x10
+	mov ax, 0x0E69
+	int 0x10
+	mov ax, 0x0E67
+	int 0x10
+	mov ax, 0x0E61
+	int 0x10
+	mov ax, 0x0E74
+	int 0x10
+	mov ax, 0x0E6f
+	int 0x10
+	cli
+	hlt
 
 ;times ((0x400) - ($ - $$)) db 0x00
